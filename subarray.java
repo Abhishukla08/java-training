dc0ea6fd1ef37a7bd49650a1c2d617a4c9c96e14
Abhinav shukla <abhinavshuklaa632@gmail.com>
2026-03-12 21:46:35 +0530

    Add files via upload

---
+++ subarray.java
@@ -0,0 +1,20 @@
+public class subarray  {
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5,4,-1,7,8};
+        System.out.println("Maximum Subarray Sum: " + maxSubArray(nums));
+    }
+}
+
